cea37ea5dc95606941ea32803244df0d1cfbd824
Eduardo Morais <emorais1375@gmail.com>
2019-03-29 18:41:07 -0400

    Criar Formulario Equipe

--- src/components/App/HelloWorld.js
+++ src/components/App/HelloWorld.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import mysql from 'mysql';
-
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form'
+import Col from 'react-bootstrap/Col'
 
-// import "./HelloWorld.css";
 
 class HelloWorld extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      agendamento: []
+      equipe_nome: [], equipe_enderecamento: []
     }
 
     this.handleOnClick = this.handleOnClick.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
+ 
   handleOnClick(){
     console.log('handleOnClick');
     let connection = mysql.createConnection({
       host     : 'localhost',
       user     : 'root',
-      password : 'password',
-      database : 'arko_db_v2'
+      password : null,
+      database : 'db_teste'
     });
 
     // connect to mysql
@@ -34,9 +36,9 @@ class HelloWorld extends React.Component {
     });
 
     // Perform a query
-    let query = 'SELECT id, contrato_id, data_agendamento, hora_agendamento, usuario_id, agendamento_status FROM agendamento LIMIT 10';
+    let query_nome = 'SELECT i.id, u.nome, e.descricao, u.id FROM inventario i, usuario u, usuario_enderecamento ue, enderecamento e WHERE i.id = 6 AND u.cargo = "INVENTARIANTE" AND ue.usuario_id = u.id AND ue.usuario_id = e.inventario_id GROUP by u.nome';
 
-    connection.query(query, (error, results, fields) => {
+    connection.query(query_nome, (error, results, fields) => {
         if(error){
             console.log("An error ocurred performing the query.");
             console.log(error);
@@ -45,53 +47,104 @@ class HelloWorld extends React.Component {
 
         console.log(results);
         this.setState({
-          agendamento: results
+          equipe_nome: results
         });
 
         console.log("Query succesfully executed");
     });
+    let query_enderecamento = 'SELECT i.id, u.nome, e.descricao, u.id FROM inventario i, usuario u, usuario_enderecamento ue, enderecamento e WHERE i.id = 6 AND u.cargo = "INVENTARIANTE" AND ue.usuario_id = u.id AND ue.usuario_id = e.inventario_id';
 
+    connection.query(query_enderecamento, (error, results, fields) => {
+        if(error){
+            console.log("An error ocurred performing the query.");
+            console.log(error);
+            return;
+        }
+
+        console.log(results);
+        this.setState({
+          equipe_enderecamento: results
+        });
+
+        console.log("Query succesfully executed");
+    });
     // Close the connection
     connection.end( () => {
         // The connection has been closed
     });
   }
+  handleSubmit(event) {
+    console.log('handleSubmit');
+    let connection = mysql.createConnection({
+      host     : 'localhost',
+      user     : 'root',
+      password : null,
+      database : 'db_teste'
+    });
 
-  render(){
+    // connect to mysql
+    connection.connect((err) => {
+      // in case of error
+      if(err){
+          console.log(err.code);
+          console.log(err.fatal);
+      }
+      console.log('conectou!');
+    });
+
+    // Perform a query
+    //let query_insert = 'INSERT INTO usuario_enderecamento (id, inventario_id, usuario_id, enderecamento_id) VALUES (NULL, inventario_id, usuario_id, enderecamento_id);';
+
+    connection.query(query_insert, (error, results, fields) => {
+        if(error){
+            console.log("An error ocurred performing the query.");
+            console.log(error);
+            return;
+        }
+
+        console.log("Query succesfully executed");
+    });
+      // Close the connection
+      connection.end( () => {
+        // The connection has been closed
+    });
+  }
+  render() {
     return(
       <div>
-        <h1>Connecting to MySQL</h1>
-        <Button variant="dark" onClick={this.handleOnClick}>Retrieve 10 first rows in the database</Button>
-        <table className="table">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Gerente ID</th>
-                        <th>Status</th>
-                        <th>Contrato ID</th>
-                        <th>Data</th>
-                        <th>Hora</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {this.state.agendamento.map(prop => {
+        <h1>Equipe</h1>
+        <Button variant="dark" onClick={this.handleOnClick}>Carregar</Button>
+        <Form>
+          <Form.Row>
+              <Col>
+                <Form.Group controlId="exampleForm.ControlSelect1">
+                  <Form.Label>INVENTARIANTE</Form.Label>
+                  <Form.Control as="select">
+                    {this.state.equipe_nome.map(prop => {
                       return(
-                        <tr key={prop.id}>
-                          <td>{prop.id}</td>
-                          <td>{prop.usuario_id}</td>
-                          <td>{prop.agendamento_status}</td>
-                          <td>{prop.contrato_id}</td>
-                          <td>{prop.data_agendamento}</td>
-                          <td>{prop.hora_agendamento}</td>
-                        </tr>
+                        <option key={prop.nome}>{prop.nome}</option>
                       );
-                    })}
-                    
-                </tbody>
-            </table>
+                    })}                  
+                  </Form.Control>
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group controlId="exampleForm.ControlSelect2">
+                    <Form.Label>FAB. INICIAL</Form.Label>
+                    <Form.Control type="number"></Form.Control>
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group controlId="exampleForm.ControlSelect3">
+                    <Form.Label>FAB.FINAL</Form.Label>
+                    <Form.Control type="number"></Form.Control>
+                </Form.Group>
+              </Col>
+              <Col><Button type="submit" onClick={this.handleOnClick}>+</Button></Col>
+          </Form.Row>
+        </Form>
       </div>
     );
   }
 }
-
 export default HelloWorld;
