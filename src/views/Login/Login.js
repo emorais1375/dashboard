f3c1964f110f032d3a98e55f2d95ebd32852f6e8
Eduardo Morais <emorais1375@gmail.com>
2019-04-10 14:33:14 -0400

    Correcoes Login Page

--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -1,45 +1,61 @@
 import React, { Component } from "react";
 import MaskedFormControl from 'react-bootstrap-maskedinput'
-import Card from 'react-bootstrap/Card'
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
+import {
+    Button,
+    Form,
+    Card
+  } from 'react-bootstrap'
 
 class Login extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            cpf='', password=''
+            cpf:'', password:'',validated: false 
         };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);  
       }
-    render() {
-    return (
-      <div className="col-md-3">
-        <br/>
-        <br/>
-            <Card bg="light" style={{ width: '18rem' }}>
-                <Card.Header>Login</Card.Header>
-                <Card.Body className="p-3">
-                    <Form>
-                        <Form.Group controlId="formBasicCPF">
-                            <Form.Label>CPF</Form.Label>
-                            <MaskedFormControl type="text" placeholder="___.___.___-__" name="cpf" mask='111.111.111-11' onChange={this.handleChange} value={this.state.cpf}/>
-                        </Form.Group>
+    handleChange(event) {
+        console.log(event.target.value)
+        console.log(event.target.name)
+        this.setState({
+            [event.target.name]: event.target.value
+          });
 
-                        <Form.Group controlId="formBasicSenha">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="******" />
-                        </Form.Group>
-                        <Button variant="primary" type="submit">
-                            Entrar
-                        </Button>
-                    </Form>
-                </Card.Body>
-            </Card>
-            <br />
-      </div>
-    );
+
+    }
+    handleSubmit(event) {
+        event.preventDefault();
+        console.log(
+            this.state.cpf,
+            this.state.password
+          );
+    }
+    render() {
+        return (
+        <div className="col-md-3">
+            <br/>
+            <br/>
+                <Card bg="light" style={{ width: '18rem' }}>
+                    <Card.Header>Login</Card.Header>
+                    <Card.Body className="p-3">
+                        <Form onSubmit={e => this.handleSubmit(e)}>
+                            <Form.Group controlId="formBasicCPF">
+                                <Form.Label>CPF</Form.Label>
+                                <MaskedFormControl type="text" placeholder="___.___.___-__" mask='111.111.111-11' name="cpf" onChange={this.handleChange} value={this.state.cpf} />
+                            </Form.Group>
+
+                            <Form.Group controlId="formBasicSenha">
+                                <Form.Label>Senha</Form.Label>
+                                <Form.Control  type="password" placeholder="******" name="password" onChange={this.handleChange} value={this.state.password}/>
+                            </Form.Group>
+                            <Button variant="info" type="submit" value="Entrar">Entrar</Button>
+                        </Form>
+                    </Card.Body>
+                </Card>
+                <br />
+        </div>
+        );
   }
 }
 
