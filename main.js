97488488187a004799593a36ac9b718d33dd4a5d
Eduardo Morais <emorais1375@gmail.com>
2019-04-14 18:33:09 -0400

    Mostrar aplicaçao

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ function createWindow() {
 
   // Don't show until we are ready and loaded
   mainWindow.once('ready-to-show', () => {
-    // mainWindow.show();
+    mainWindow.show();
     // Open the DevTools automatically if developing
     if ( dev ) {
       // mainWindow.webContents.openDevTools();
