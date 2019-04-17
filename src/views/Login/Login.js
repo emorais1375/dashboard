e05f6c517563f73935a180090043f1e87b9a77c5
Eduardo Morais <emorais1375@gmail.com>
2019-04-17 16:51:51 -0400

    Receber usuario_id no login

--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -1,89 +1,64 @@
-import React, { Component } from "react";
-import mysql from 'mysql';
+import React, { Component } from "react"
+import mysql from 'mysql'
 import env from '../../../.env'
 import MaskedFormControl from 'react-bootstrap-maskedinput'
 import {
-    Button,
-    Form,
-    Card
-  } from 'react-bootstrap'
+  Button,
+  Form,
+  Card,
+  ButtonToolbar
+} from 'react-bootstrap'
 
 class Login extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            cpf:'', password:'',usuario:[]
-        };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);  
-      }
-    handleChange(event) {
-        this.setState({
-            [event.target.name]: event.target.value
-          });
+  constructor(props) {
+    super(props)
+    this.state = {
+      cpf:'', password:'', usuario:[]
     }
-    handleSubmit (event) {
-        event.preventDefault();
-        console.log(
-            this.state.cpf,
-            this.state.password
-          );
-          let cpf = this.state.cpf;
-          let pass = this.state.password;
-          let connection = mysql.createConnection(env.config_mysql);
-          // connect to mysql
-        connection.connect((err) => {
-            // in case of error
-            if(err){
-                console.log(err.code);
-                console.log(err.fatal);
-            }
-            console.log('conectou!');
-        });
-  
-          // Perform a query
-        let query = 'SELECT l.cpf,l.password, l.usuario_id FROM login l WHERE l.cpf = ? AND l.password = ?';
-  
-        connection.query(query,[cpf,pass], (error, results, fields) => {
-            if(error){
-                console.log("An error ocurred performing the query.");
-                console.log(error);
-                return;
-            }
-            this.setState({
-                usuario: results
-            });
-            if(results.length){
-                console.log(results);
-                console.log("Encontrado");
-                let query2 = 'SELECT i.id FROM inventario i WHERE i.usuario_resp_cadastro_inventario_id = ?';
-            
-                connection.query(query2,[this.state.usuario[0].usuario_id], (error, results2, fields) => {
-                    if(error){
-                        console.log("An error ocurred performing the query.");
-                        console.log(error);
-                        return;
-                    }                                   
-                    console.log("Query 2 succesfully executed");
-                    console.log("Vc tem um inventario");
-                    console.log(results2);
-                    //this.props.history.push("/dashboard");  
-                    <Redirect to="/dashboard"/>               
-                });
-                // Close the connection
-                connection.end( () => {});
-            }else{
-                console.log(results);
-                console.log('Nao encontrado');
-                // Close the connection
-                connection.end( () => {});
-            }        
-            
-            console.log("Query 1 succesfully executed");
-          });
-         
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(e) {
+    const target = e.target
+    const value = target.value
+    const name = target.name
+    this.setState({[name]: value})
+  }
+
+  handleSubmit (e) {
+    e.preventDefault()
+    let {cpf, password} = this.state
+    if (cpf && password) {
+      let connection = mysql.createConnection(env.config_mysql)
+      let query = `
+        SELECT usuario_id 
+        FROM login 
+        WHERE cpf=? 
+        AND password=? 
+        AND login_status='ATIVO'
+      `
+    
+      connection.query(query, [cpf, password], (error, results, fields) => {
+        if(error){
+          console.log(error.code,error.fatal)
+          return
+        }
+        if (results.length) {
+          console.log(results[0].usuario_id)
+        }
+        else {
+          console.log('CPF:'+cpf+' ou Senha:'+password+' inválida!')
+        }
+        connection.end()
+      })
+    } else {
+      console.log('CPF:'+cpf+' ou Senha:'+password+' inválida!')
     }
-    render() {
+  }
+
+  render() {
+        const { validated, cpf, password } = this.state;
         return (
         <div className="col-md-3">
             <br/>
@@ -91,17 +66,28 @@ class Login extends Component {
                 <Card bg="light" style={{ width: '18rem' }}>
                     <Card.Header>Login</Card.Header>
                     <Card.Body className="p-3">
-                        <Form onSubmit={e => this.handleSubmit(e)}>
-                            <Form.Group controlId="formBasicCPF">
+                        <Form onSubmit={this.handleSubmit}>
+                            <Form.Group>
                                 <Form.Label>CPF</Form.Label>
-                                <MaskedFormControl type="text" placeholder="___.___.___-__" mask='111.111.111-11' name="cpf" onChange={this.handleChange} value={this.state.cpf} />
+                                <MaskedFormControl type="numbe" 
+                                    placeholder="CPF" mask='111.111.111-11' 
+                                    name="cpf" onChange={this.handleChange} 
+                                    value={cpf}
+                                />
                             </Form.Group>
 
-                            <Form.Group controlId="formBasicSenha">
+                            <Form.Group>
                                 <Form.Label>Senha</Form.Label>
-                                <Form.Control  type="password" placeholder="******" name="password" onChange={this.handleChange} value={this.state.password}/>
+                                <Form.Control type="password" 
+                                    placeholder="******" name="password" 
+                                    onChange={this.handleChange} 
+                                    value={password}
+                                />
                             </Form.Group>
-                            <Button variant="info" type="submit" value="Entrar">Entrar</Button>
+
+                            <ButtonToolbar>
+                                <Button as="input" type="submit" value="Entrar" block />
+                            </ButtonToolbar>
                         </Form>
                     </Card.Body>
                 </Card>
